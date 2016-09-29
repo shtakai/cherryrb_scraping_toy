2eddc296216397b5dd96ed5ac89792ced540930f
kytiken <kytiken@gmail.com>
2016-09-29 22:57:07 +0900

    add benchmark

--- app.rb
+++ app.rb
@@ -1,25 +1,32 @@
 require 'bundler/setup'
 require 'nokogiri'
 require 'open-uri'
+require 'benchmark'
 
-host = 'http://rubykaigi.org'
-path = '/2016/schedule/'
-url = host + path
+benchmark_result = Benchmark.realtime do
 
-url_list = []
-open(url) do |f|
-  doc = Nokogiri::HTML(f.read)
-  doc.css('table a').each do |a|
-    url_list << host + a['href']
-  end
-end
+  host = 'http://rubykaigi.org'
+  path = '/2016/schedule/'
+  url = host + path
 
-url_list.each do |url|
-  p url
+  url_list = []
   open(url) do |f|
     doc = Nokogiri::HTML(f.read)
-    doc.css('.section p').each do |g|
-      p g.text
+    doc.css('table a').each do |a|
+      url_list << host + a['href']
     end
   end
+
+  url_list.each do |url|
+    p url
+    open(url) do |f|
+      doc = Nokogiri::HTML(f.read)
+      doc.css('.section p').each do |g|
+        p g.text
+      end
+    end
+  end
+
 end
+
+puts benchmark_result
